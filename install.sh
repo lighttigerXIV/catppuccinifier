5238397af79c1cd03dce38e8283654436cdf4522
lighttigerxiv <lighttigerxiv@gmail.com>
2023-03-20 21:59:05 +0000

    removed dependency check in install

--- install.sh
+++ install.sh
@@ -1,25 +1,18 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-if ! which magick >/dev/null; then
+if [[ "$*" == "--local" ]] || [[ "$*" == "-l" ]]; then
 
-    echo "Please install image magick"
+    cp $SCRIPT_DIR/bin/catppuccinifier $HOME/.local/bin/
 
 else
 
-    if [[ "$*" == "--local" ]] || [[ "$*" == "-l" ]]; then
+    sudo cp -p -r $SCRIPT_DIR/bin/catppuccinifier /usr/local/bin/
 
-        cp $SCRIPT_DIR/bin/catppuccinifier $HOME/.local/bin/
-        
-    else
-
-        sudo cp -p -r $SCRIPT_DIR/bin/catppuccinifier /usr/local/bin/
-
-    fi
-
-    if ! [ -d $HOME/.local/share/catppuccinifier ]; then
-
-        mkdir $HOME/.local/share/catppuccinifier
-    fi
-
-    cp -p -r $SCRIPT_DIR/src/flavors/ $HOME/.local/share/catppuccinifier/
 fi
+
+if ! [ -d $HOME/.local/share/catppuccinifier ]; then
+
+    mkdir $HOME/.local/share/catppuccinifier
+fi
+
+cp -p -r $SCRIPT_DIR/src/flavors/ $HOME/.local/share/catppuccinifier/
