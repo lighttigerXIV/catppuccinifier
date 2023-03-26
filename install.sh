955401b3b5c048f8ed356a81182dc7a9507f7132
lighttigerxiv <lighttigerxiv@gmail.com>
2023-03-26 16:56:57 +0100

    fixed desktop entry

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ if [[ "$*" == "--local" ]] || [[ "$*" == "-l" ]]; then
 
     cp "$SCRIPT_DIR"/bin/catppuccinifier "$HOME"/.local/bin/
     cp "$SCRIPT_DIR"/bin/catppuccinifier-gui "$HOME"/.local/bin/
+    sed -i "s|Exec=.*|Exec=$HOME/.local/bin/catppuccinifier-gui|g" desktop/Catppuccinifier.desktop 
 
 else
 
     sudo cp -p -r "$SCRIPT_DIR"/bin/catppuccinifier /usr/local/bin/
     sudo cp -p -r "$SCRIPT_DIR"/bin/catppuccinifier-gui /usr/local/bin/
+    sed -i "s|Exec=.*|Exec=/usr/local/bin/catppuccinifier-gui|g" desktop/Catppuccinifier.desktop 
 
 fi
 
@@ -21,5 +23,5 @@ fi
 
 cp -p -r "$SCRIPT_DIR"/src/flavors/ "$HOME"/.local/share/catppuccinifier/
 cp "$SCRIPT_DIR"/images/catppuccinifier.png "$HOME"/.local/share/icons/hicolor/512x512/apps/
-sed -i "s|Icon=.*|Icon=$HOME/.local/share/icons/hicolor/512x512/catppuccinifier.png|g" desktop/Catppuccinifier.desktop 
+sed -i "s|Icon=.*|Icon=$HOME/.local/share/icons/hicolor/512x512/apps/catppuccinifier.png|g" desktop/Catppuccinifier.desktop
 cp "$SCRIPT_DIR/desktop/Catppuccinifier.desktop" "$HOME/.local/share/applications/"
