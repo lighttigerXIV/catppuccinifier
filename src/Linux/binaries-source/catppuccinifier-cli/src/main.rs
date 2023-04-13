ebdeef4444a45dfbe11e3c7c821e13a799310536
lighttigerxiv <lighttigerxiv@gmail.com>
2023-04-13 16:02:41 +0100

    updated cli to use clap and cleaned the code

--- src/Linux/binaries-source/catppuccinifier-cli/src/main.rs
+++ src/Linux/binaries-source/catppuccinifier-cli/src/main.rs
@@ -1,159 +1,147 @@
 use std::env;
+use std::path::PathBuf;
 use std::process::Command;
+use clap::{command, ArgAction, Arg, value_parser};
+use clap::parser::ValuesRef;
 
+
+fn get_cli() -> clap::Command {
+    command!()
+        .version("3.0")
+        .name("Catppuccinifier")
+        .about("A cli tool to catppuccinify your wallpapers")
+        .author("lighttigerXIV")
+        .arg(
+            Arg::new("flavor")
+                .short('f')
+                .long("flavor")
+                .action(ArgAction::Set)
+                .value_name("FLAVOR")
+                .num_args(1..6)
+                .help("Selects the flavor to apply to the image")
+                .value_delimiter(' ')
+                .value_parser(["latte", "frappe", "macchiato", "mocha", "oled", "all"])
+                .default_value("all")
+        )
+        .arg(
+            Arg::new("noise")
+                .short('n')
+                .long("noise")
+                .action(ArgAction::Set)
+                .value_name("NOISE")
+                .num_args(1)
+                .help("Selects the noise level to apply to the image")
+                .value_parser(["0", "1", "2", "3", "4"])
+                .default_value("4")
+        )
+        .arg(
+            Arg::new("image")
+                .short('i')
+                .long("image")
+                .action(ArgAction::Set)
+                .value_name("IMAGE")
+                .help("Selects the image to apply the flavor")
+                .required(true)
+        )
+}
+
+fn convert_image(
+    flavor: &str,
+    noise: &str,
+    image: &str,
+    home_folder_path: &str,
+    exec_folder_path: &str,
+) {
+    println!("Generating {} image with noise level {}", flavor, noise);
+
+    let command = format!(
+        "convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/{}.png' -hald-clut '{}-{}'",
+        exec_folder_path, image, home_folder_path, noise, flavor, flavor, image
+    );
+
+    Command::new("/bin/sh")
+        .arg("-c")
+        .arg(&command)
+        .output()
+        .expect("ERROR: Couldn't convert image");
+}
 
 
 fn main() {
+    let matches = get_cli().get_matches();
+    let flavors_reference = matches.get_many::<String>("flavor");
+    let noise_level = matches.get_one::<String>("noise");
+    let image = matches.get_one::<String>("image");
+
+
     match env::current_dir() {
         Ok(exec_folder) => {
-
             match dirs::home_dir() {
-                Some(home_folder) =>{
-
-                    let arguments: Vec<String> = env::args().collect();
+                Some(home_folder) => {
                     let exec_folder_path = exec_folder.to_str().unwrap();
                     let home_folder_path = home_folder.to_str().unwrap();
 
-                    let mut noise_level = 0;
+                    match flavors_reference {
+                        Some(flavors) => {
+                            for flavor in flavors {
+                                if flavor != "all" {
+                                    convert_image(
+                                        flavor,
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+                                }
 
-                    //Gets noise level
-                    if arguments.iter().any(|argument| argument == "n1" || argument == "noise1"){
-                        noise_level = 1;
+                                if flavor == "all" {
+                                    println!("Generating images with all flavors with noise level {}", noise_level.unwrap());
+
+                                    convert_image(
+                                        "latte",
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+
+                                    convert_image(
+                                        "frappe",
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+
+                                    convert_image(
+                                        "macchiato",
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+
+                                    convert_image(
+                                        "mocha",
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+
+                                    convert_image(
+                                        "oled",
+                                        noise_level.unwrap(),
+                                        image.unwrap(),
+                                        home_folder_path,
+                                        exec_folder_path,
+                                    );
+                                }
+                            }
+                        }
+                        None => {}
                     }
-
-                    if arguments.iter().any(|argument| argument == "n2" || argument == "noise2"){
-                        noise_level = 2;
-                    }
-
-                    if arguments.iter().any(|argument| argument == "n3" || argument == "noise3"){
-                        noise_level = 3;
-                    }
-
-                    if arguments.iter().any(|argument| argument == "n4" || argument == "noise4"){
-                        noise_level = 4;
-                    }
-
-                    let image = &arguments[arguments.len() - 1];
-
-                    let mut i = 0;
-
-                    while i < arguments.len(){
-
-                        let argument = &arguments[i];
-
-                        if argument == "latte" || argument == "-l"{
-
-                            println!("Generating latte image with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/latte.png' -hald-clut 'latte-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to latte");
-                        }
-
-                        if argument == "frappe" || argument == "-f"{
-
-                            println!("Generating frappe image with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/frappe.png' -hald-clut 'frappe-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to frappe");
-                        }
-
-                        if argument == "macchiato" || argument == "-ma"{
-
-                            println!("Generating macchiato image with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/macchiato.png' -hald-clut 'macchiato-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to macchiato");
-                        }
-
-                        if argument == "mocha" || argument == "-mo"{
-
-                            println!("Generating mocha image with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/mocha.png' -hald-clut 'mocha-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to mocha");
-                        }
-
-                        if argument == "oled" || argument == "-o"{
-
-                            println!("Generating oled image with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/oled.png' -hald-clut 'oled-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to oled");
-                        }
-
-                        if argument == "all" || argument == "-a"{
-
-                            println!("Generating images with all flavors with noise level {}", noise_level);
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/latte.png' -hald-clut 'latte-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to latte");
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/frappe.png' -hald-clut 'frappe-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to frappe");
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/macchiato.png' -hald-clut 'macchiato-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to macchiato");
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/mocha.png' -hald-clut 'mocha-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to mocha");
-
-                            let command = format!("convert '{}/{}' '{}/.local/share/catppuccinifier/flavors/noise-{}/oled.png' -hald-clut 'oled-{}'", exec_folder_path, image, home_folder_path, noise_level, image);
-
-                            Command::new("/bin/sh")
-                                .arg("-c")
-                                .arg(&command)
-                                .output()
-                                .expect("ERROR: Couldn't convert image to oled");
-                        }
-
-                        i += 1;
-                    }
-                },
+                }
                 None => {
                     println!("ERROR: Couldn't get home folder");
                 }
