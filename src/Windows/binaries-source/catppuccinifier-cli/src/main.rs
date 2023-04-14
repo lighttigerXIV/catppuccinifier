bb94471ea1ed03903d73b77c2b40f937190845bc
lighttigerxiv <lighttigerxiv@gmail.com>
2023-04-14 17:12:41 +0100

    changed cli to use clap in windows

--- src/Windows/binaries-source/catppuccinifier-cli/src/main.rs
+++ src/Windows/binaries-source/catppuccinifier-cli/src/main.rs
@@ -1,255 +1,131 @@
 use std::env;
 use std::process::Command;
 use substring::Substring;
+use clap::{command, ArgAction, Arg};
+
+fn get_cli() -> clap::Command {
+    command!()
+        .version("3.0")
+        .name("Catppuccinifier")
+        .about("A cli tool to catppuccinify your wallpapers")
+        .author("lighttigerXIV")
+        .arg(
+            Arg::new("flavor")
+                .short('f')
+                .long("flavor")
+                .action(ArgAction::Set)
+                .value_name("FLAVOR")
+                .num_args(1..6)
+                .help("Selects the flavor to apply to the image")
+                .value_delimiter(' ')
+                .value_parser(["latte", "frappe", "macchiato", "mocha", "oled", "all"])
+                .default_value("all")
+        )
+        .arg(
+            Arg::new("noise")
+                .short('n')
+                .long("noise")
+                .action(ArgAction::Set)
+                .value_name("NOISE")
+                .num_args(1)
+                .help("Selects the noise level to apply to the image")
+                .value_parser(["0", "1", "2", "3", "4"])
+                .default_value("4")
+        )
+        .arg(
+            Arg::new("image")
+                .short('i')
+                .long("image")
+                .action(ArgAction::Set)
+                .value_name("IMAGE")
+                .help("Selects the image to apply the flavor")
+                .required(true)
+        )
+}
+
+fn convert_image(
+    flavor: &str,
+    noise_level: &str,
+    image: &str,
+    current_folder_path: &str,
+    catppuccinifier_folder_path: &str,
+) {
+
+    println!("Generating {} image with noise level {}", flavor, noise_level);
+
+    let command = format!(
+        r"magick convert '{}\{}' '{}\flavors\noise-{}\{}.png' -hald-clut '{}\{}-{}'",
+        current_folder_path,
+        image,
+        catppuccinifier_folder_path,
+        noise_level,
+        flavor,
+        current_folder_path,
+        flavor,
+        image
+    );
+
+    let flavor_command = Command::new("powershell")
+        .arg("-Command")
+        .arg(&command)
+        .output()
+        .expect("ERROR: Couldn't convert image");
+
+    if !flavor_command.status.success() {
+        println!("An error occurred: {}", String::from_utf8_lossy(&flavor_command.stderr));
+    }
+}
 
 fn main() {
+    let matches = get_cli().get_matches();
+    let flavors_reference = matches.get_many::<String>("flavor");
+    let noise_level = matches.get_one::<String>("noise");
+    let image_path = matches.get_one::<String>("image");
+
     match env::current_dir() {
         Ok(current_folder) => {
-            let arguments: Vec<String> = env::args().collect();
             let current_folder_path = current_folder.to_str().unwrap();
             let catppuccinifier_folder_path = r"C:\Program Files\Catppuccinifier";
 
-            let mut noise_level = 0;
+            let mut image= "";
 
-            //Gets noise level
-            if arguments
-                .iter()
-                .any(|argument| argument == "n1" || argument == "noise1")
-            {
-                noise_level = 1;
+            if image_path.unwrap().contains(r".\") {
+                image = image_path.unwrap().substring(2, image_path.unwrap().len());
+            } else {
+                image = image_path.unwrap()
             }
 
-            if arguments
-                .iter()
-                .any(|argument| argument == "n2" || argument == "noise2")
-            {
-                noise_level = 2;
-            }
+            match flavors_reference {
+                Some(flavors) => {
+                    for flavor in flavors {
+                        if flavor != "all" {
+                            convert_image(
+                                flavor,
+                                noise_level.unwrap(),
+                                image,
+                                current_folder_path,
+                                catppuccinifier_folder_path,
+                            );
+                        }
 
-            if arguments
-                .iter()
-                .any(|argument| argument == "n3" || argument == "noise3")
-            {
-                noise_level = 3;
-            }
+                        if flavor == "all"{
 
-            if arguments
-                .iter()
-                .any(|argument| argument == "n4" || argument == "noise4")
-            {
-                noise_level = 4;
-            }
+                            let possible_flavors = ["latte", "frappe", "macchiato", "mocha", "oled"];
 
-            let mut image = String::from(&arguments[arguments.len() - 1]);
+                            for possible_flavor in possible_flavors{
 
-            if image.contains(r".\"){
-                image = image.substring(2, image.len()).to_string();
-            }
-
-            let mut i = 0;
-
-            while i < arguments.len() {
-                let argument = &arguments[i];
-
-                if argument == "latte" || argument == "-l" {
-                    println!("Generating latte image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\latte.png' -hald-clut '{}\latte-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to latte");
+                                convert_image(
+                                    possible_flavor,
+                                    noise_level.unwrap(),
+                                    image,
+                                    current_folder_path,
+                                    catppuccinifier_folder_path,
+                                );
+                            }
+                        }
+                    }
                 }
-
-                if argument == "frappe" || argument == "-f" {
-                    println!("Generating frappe image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\frappe.png' -hald-clut '{}\frappe-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to frappe");
-                }
-
-                if argument == "macchiato" || argument == "-ma" {
-                    println!(
-                        "Generating macchiato image with noise level {}",
-                        noise_level
-                    );
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\macchiato.png' -hald-clut '{}\macchiato-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to macchiato");
-                }
-
-                if argument == "mocha" || argument == "-mo" {
-                    println!("Generating mocha image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\mocha.png' -hald-clut '{}\mocha-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to mocha");
-                }
-
-                if argument == "oled" || argument == "-o" {
-                    println!("Generating oled image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\oled.png' -hald-clut '{}\oled-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to oled");
-                }
-
-                if argument == "all" || argument == "-a" {
-                    println!("Generating latte image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\latte.png' -hald-clut '{}\latte-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to latte");
-
-                    println!("Generating frappe image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\frappe.png' -hald-clut '{}\frappe-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to frappe");
-
-                    println!(
-                        "Generating macchiato image with noise level {}",
-                        noise_level
-                    );
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\macchiato.png' -hald-clut '{}\macchiato-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to macchiato");
-
-                    println!("Generating mocha image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\mocha.png' -hald-clut '{}\mocha-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to mocha");
-
-                    println!("Generating oled image with noise level {}", noise_level);
-
-                    let command = format!(
-                        r"magick convert '{}\{}' '{}\flavors\noise-{}\oled.png' -hald-clut '{}\oled-{}'",
-                        current_folder_path,
-                        image,
-                        catppuccinifier_folder_path,
-                        noise_level,
-                        current_folder_path,
-                        image
-                    );
-
-                    Command::new("powershell")
-                        .arg("-Command")
-                        .arg(&command)
-                        .output()
-                        .expect("ERROR: Couldn't convert image to oled");
-                }
-
-                i += 1;
+                None => {}
             }
         }
         Err(_) => {
