705feab7f2dbdb81a47ab8588905aa698fd3e139
lighttigerxiv <lighttigerxiv@gmail.com>
2023-04-23 18:49:37 +0100

    changed buttons to know which flavors is being saved

--- src/Windows/binaries-source/catppuccinifier-gui/Catppuccinifier/MainForm.Designer.cs
+++ src/Windows/binaries-source/catppuccinifier-gui/Catppuccinifier/MainForm.Designer.cs
@@ -99,7 +99,7 @@
             this.PropertiesPanel.Location = new System.Drawing.Point(0, 0);
             this.PropertiesPanel.Margin = new System.Windows.Forms.Padding(0);
             this.PropertiesPanel.Name = "PropertiesPanel";
-            this.PropertiesPanel.Size = new System.Drawing.Size(343, 736);
+            this.PropertiesPanel.Size = new System.Drawing.Size(343, 846);
             this.PropertiesPanel.TabIndex = 0;
             // 
             // GenerateImagesButton
@@ -208,9 +208,9 @@
             this.PreviewLatteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PreviewLatteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PreviewLatteButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
-            this.PreviewLatteButton.Location = new System.Drawing.Point(162, 9);
+            this.PreviewLatteButton.Location = new System.Drawing.Point(210, 9);
             this.PreviewLatteButton.Name = "PreviewLatteButton";
-            this.PreviewLatteButton.Size = new System.Drawing.Size(137, 33);
+            this.PreviewLatteButton.Size = new System.Drawing.Size(90, 33);
             this.PreviewLatteButton.TabIndex = 7;
             this.PreviewLatteButton.Text = "Preview";
             this.PreviewLatteButton.UseVisualStyleBackColor = false;
@@ -225,9 +225,9 @@
             this.SaveLatteButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
             this.SaveLatteButton.Location = new System.Drawing.Point(19, 9);
             this.SaveLatteButton.Name = "SaveLatteButton";
-            this.SaveLatteButton.Size = new System.Drawing.Size(137, 33);
+            this.SaveLatteButton.Size = new System.Drawing.Size(184, 33);
             this.SaveLatteButton.TabIndex = 6;
-            this.SaveLatteButton.Text = "Save";
+            this.SaveLatteButton.Text = "Save Latte";
             this.SaveLatteButton.UseVisualStyleBackColor = false;
             this.SaveLatteButton.Click += new System.EventHandler(this.SaveLatteButton_Click);
             // 
@@ -237,7 +237,7 @@
             this.GeneratedFrappePanel.Controls.Add(this.GeneratedFrappeBox);
             this.GeneratedFrappePanel.Controls.Add(this.PreviewFrappeButton);
             this.GeneratedFrappePanel.Controls.Add(this.SaveFrappeButton);
-            this.GeneratedFrappePanel.Location = new System.Drawing.Point(356, 374);
+            this.GeneratedFrappePanel.Location = new System.Drawing.Point(356, 383);
             this.GeneratedFrappePanel.Name = "GeneratedFrappePanel";
             this.GeneratedFrappePanel.Size = new System.Drawing.Size(316, 345);
             this.GeneratedFrappePanel.TabIndex = 9;
@@ -260,9 +260,9 @@
             this.PreviewFrappeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PreviewFrappeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PreviewFrappeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
-            this.PreviewFrappeButton.Location = new System.Drawing.Point(162, 9);
+            this.PreviewFrappeButton.Location = new System.Drawing.Point(209, 9);
             this.PreviewFrappeButton.Name = "PreviewFrappeButton";
-            this.PreviewFrappeButton.Size = new System.Drawing.Size(137, 33);
+            this.PreviewFrappeButton.Size = new System.Drawing.Size(90, 33);
             this.PreviewFrappeButton.TabIndex = 7;
             this.PreviewFrappeButton.Text = "Preview";
             this.PreviewFrappeButton.UseVisualStyleBackColor = false;
@@ -277,9 +277,9 @@
             this.SaveFrappeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
             this.SaveFrappeButton.Location = new System.Drawing.Point(19, 9);
             this.SaveFrappeButton.Name = "SaveFrappeButton";
-            this.SaveFrappeButton.Size = new System.Drawing.Size(137, 33);
+            this.SaveFrappeButton.Size = new System.Drawing.Size(184, 33);
             this.SaveFrappeButton.TabIndex = 6;
-            this.SaveFrappeButton.Text = "Save";
+            this.SaveFrappeButton.Text = "Save Frappe";
             this.SaveFrappeButton.UseVisualStyleBackColor = false;
             this.SaveFrappeButton.Click += new System.EventHandler(this.SaveFrappeButton_Click);
             // 
@@ -312,9 +312,9 @@
             this.PreviewMacchiatoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PreviewMacchiatoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PreviewMacchiatoButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
-            this.PreviewMacchiatoButton.Location = new System.Drawing.Point(162, 9);
+            this.PreviewMacchiatoButton.Location = new System.Drawing.Point(209, 9);
             this.PreviewMacchiatoButton.Name = "PreviewMacchiatoButton";
-            this.PreviewMacchiatoButton.Size = new System.Drawing.Size(137, 33);
+            this.PreviewMacchiatoButton.Size = new System.Drawing.Size(90, 33);
             this.PreviewMacchiatoButton.TabIndex = 7;
             this.PreviewMacchiatoButton.Text = "Preview";
             this.PreviewMacchiatoButton.UseVisualStyleBackColor = false;
@@ -329,9 +329,9 @@
             this.SaveMacchiatoButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
             this.SaveMacchiatoButton.Location = new System.Drawing.Point(19, 9);
             this.SaveMacchiatoButton.Name = "SaveMacchiatoButton";
-            this.SaveMacchiatoButton.Size = new System.Drawing.Size(137, 33);
+            this.SaveMacchiatoButton.Size = new System.Drawing.Size(184, 33);
             this.SaveMacchiatoButton.TabIndex = 6;
-            this.SaveMacchiatoButton.Text = "Save";
+            this.SaveMacchiatoButton.Text = "Save Macchiato";
             this.SaveMacchiatoButton.UseVisualStyleBackColor = false;
             this.SaveMacchiatoButton.Click += new System.EventHandler(this.SaveMacchiatoButton_Click);
             // 
@@ -341,7 +341,7 @@
             this.GeneratedMochaPanel.Controls.Add(this.GeneratedMochaBox);
             this.GeneratedMochaPanel.Controls.Add(this.PreviewMochaButton);
             this.GeneratedMochaPanel.Controls.Add(this.SaveMochaButton);
-            this.GeneratedMochaPanel.Location = new System.Drawing.Point(695, 374);
+            this.GeneratedMochaPanel.Location = new System.Drawing.Point(695, 383);
             this.GeneratedMochaPanel.Name = "GeneratedMochaPanel";
             this.GeneratedMochaPanel.Size = new System.Drawing.Size(316, 345);
             this.GeneratedMochaPanel.TabIndex = 11;
@@ -364,9 +364,9 @@
             this.PreviewMochaButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PreviewMochaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PreviewMochaButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
-            this.PreviewMochaButton.Location = new System.Drawing.Point(162, 9);
+            this.PreviewMochaButton.Location = new System.Drawing.Point(209, 9);
             this.PreviewMochaButton.Name = "PreviewMochaButton";
-            this.PreviewMochaButton.Size = new System.Drawing.Size(137, 33);
+            this.PreviewMochaButton.Size = new System.Drawing.Size(90, 33);
             this.PreviewMochaButton.TabIndex = 7;
             this.PreviewMochaButton.Text = "Preview";
             this.PreviewMochaButton.UseVisualStyleBackColor = false;
@@ -381,9 +381,9 @@
             this.SaveMochaButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
             this.SaveMochaButton.Location = new System.Drawing.Point(19, 9);
             this.SaveMochaButton.Name = "SaveMochaButton";
-            this.SaveMochaButton.Size = new System.Drawing.Size(137, 33);
+            this.SaveMochaButton.Size = new System.Drawing.Size(184, 33);
             this.SaveMochaButton.TabIndex = 6;
-            this.SaveMochaButton.Text = "Save";
+            this.SaveMochaButton.Text = "Save Mocha";
             this.SaveMochaButton.UseVisualStyleBackColor = false;
             this.SaveMochaButton.Click += new System.EventHandler(this.SaveMochaButton_Click);
             // 
@@ -416,9 +416,9 @@
             this.PreviewOledButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PreviewOledButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PreviewOledButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
-            this.PreviewOledButton.Location = new System.Drawing.Point(162, 9);
+            this.PreviewOledButton.Location = new System.Drawing.Point(209, 9);
             this.PreviewOledButton.Name = "PreviewOledButton";
-            this.PreviewOledButton.Size = new System.Drawing.Size(137, 33);
+            this.PreviewOledButton.Size = new System.Drawing.Size(90, 33);
             this.PreviewOledButton.TabIndex = 7;
             this.PreviewOledButton.Text = "Preview";
             this.PreviewOledButton.UseVisualStyleBackColor = false;
@@ -433,9 +433,9 @@
             this.SaveOledButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(214)))), ((int)(((byte)(244)))));
             this.SaveOledButton.Location = new System.Drawing.Point(19, 9);
             this.SaveOledButton.Name = "SaveOledButton";
-            this.SaveOledButton.Size = new System.Drawing.Size(137, 33);
+            this.SaveOledButton.Size = new System.Drawing.Size(184, 33);
             this.SaveOledButton.TabIndex = 6;
-            this.SaveOledButton.Text = "Save";
+            this.SaveOledButton.Text = "Save Oled";
             this.SaveOledButton.UseVisualStyleBackColor = false;
             this.SaveOledButton.Click += new System.EventHandler(this.SaveOledButton_Click);
             // 
@@ -444,7 +444,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(46)))));
-            this.ClientSize = new System.Drawing.Size(1374, 734);
+            this.ClientSize = new System.Drawing.Size(1698, 844);
             this.Controls.Add(this.GeneratedOledPanel);
             this.Controls.Add(this.GeneratedMochaPanel);
             this.Controls.Add(this.GeneratedMacchiatoPanel);
--- src/Windows/binaries-source/catppuccinifier-gui/Catppuccinifier/MainForm.cs
+++ src/Windows/binaries-source/catppuccinifier-gui/Catppuccinifier/MainForm.cs
@@ -247,5 +247,6 @@ namespace Catppuccinifier
         {
             OpenPreview("mocha");
         }
+
     }
 }
