96e5c6c102010a8dab0278f2b60a908f387ece64
lighttigerXIV <afonsofonseca14401@gmail.com>
2023-05-03 07:48:55 +0000

    fixed bugs in windows

--- src/Linux/binaries-source/catppuccinifier-cli/target/debug/deps/catppuccinfier_cli_windows-d98718a9446cba97.d
+++
@@ -1,5 +0,0 @@
-c:\Users\lighttigerxiv\Downloads\catppuccinifier\src\catppuccinfier-cli-windows\target\debug\deps\catppuccinfier_cli_windows-d98718a9446cba97.rmeta: src\main.rs
-
-c:\Users\lighttigerxiv\Downloads\catppuccinifier\src\catppuccinfier-cli-windows\target\debug\deps\catppuccinfier_cli_windows-d98718a9446cba97.d: src\main.rs
-
-src\main.rs:
