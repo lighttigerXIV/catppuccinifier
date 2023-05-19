cadea65890111faa5828f40a27c782a9cd58c0fb
lighttigerxiv <lighttigerxiv@gmail.com>
2023-05-19 20:11:45 +0100

    Updated GUI

--- src/source-code/catppuccinifier-gui/src/pages/Preview/PreviewPage.jsx
+++ src/source-code/catppuccinifier-gui/src/pages/Preview/PreviewPage.jsx
@@ -3,10 +3,11 @@ import { convertFileSrc } from "@tauri-apps/api/tauri";
 export default function PreviewPage(){
 
     const params = new URLSearchParams(window.location.search);
-    const imagePath = params.get("path")
+    const imagePath = params.get("path");
+    const theme = localStorage.getItem("theme") !== null ? localStorage.getItem("theme") : "theme-mocha";
 
     return(
-        <div className="h-screen min-w-full w-full bg-base text-text p-4 flex justify-center">
+        <div className={"h-screen min-w-full w-full bg-skin-base text-text p-4 flex justify-center " + theme}>
             <img
                 src={convertFileSrc(imagePath)}
                 className="object-contain rounded-md h-full"
--- src/source-code/catppuccinifier-gui/vite.config.js
+++ src/source-code/catppuccinifier-gui/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [svgr(), react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
